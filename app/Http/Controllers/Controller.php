1ae7fc75bc092826da30cfd5ecc5bcf6c240c5b1
TienHM <tienhm@beetsoft.com.vn>
2022-05-31 22:47:30 +0700

    feat(integrate): integrate DarkaOnLine/L5-Swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,14 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="List APIs provided by Beetsoft",
+ *      description="OpenApi or swagger specification for list api to provide for third party"
+ * )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
